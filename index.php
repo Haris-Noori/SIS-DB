898c059523917782948b1fd5cdc843b64a640730
haris-noori <harisnoori20@gmail.com>
2019-07-06 15:45:57 +0500

    INDEX_READY

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
-
-
-    echo "HELLO ADMIN, this is index page";
-
+    include "header.php"
 ?>
+
+<!DOCTYPE html>
+<head>
+    <title> Premier League | Home </title>
+    <!-- <link rel="import" href="main.html"> -->
+</head>
+<body>
+
+    <h1>This is home page</h1>
+
+    
+    
+    <!-- -->
+    <?php  include "footer.php"?>
+</body>
+</html>
