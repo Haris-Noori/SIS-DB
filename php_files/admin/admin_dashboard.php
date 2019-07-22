f3c40800ff574aa4d77e903dc9b00cc53ad3eb61
haris-noori <harisnoori20@gmail.com>
2019-07-22 21:06:44 +0500

    dashboard-ready

--- php_files/admin/admin_dashboard.php
+++ php_files/admin/admin_dashboard.php
@@ -4,14 +4,55 @@
 <html>
 <head>
     <title>Admin | Dashboard</title>
+
+    <link rel="stylesheet" type="text/css" href="../../css/admin_dashboard.css">
 </head>
 <body>
 
 
-    <hr>
+    <center>
+        <h2>Manage your site here</h2>
+        <hr>
+
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../../images/dashboard-card.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Manage fixtures</h5>
+                            <p class="card-text">Add, change and update date or time of matches</p>
+                            <a href="#" class="btn btn-info">Go to fixtures</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4"><div class="card" style="width: 18rem;">
+                        <img src="../../images/dashboard-card.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Update Points Table</h5>
+                            <p class="card-text">Remember, to update table, after each match of league</p>
+                            <a href="#" class="btn btn-info">Go to points table</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                        <img src="../../images/dashboard-card.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Update Player Stats</h5>
+                            <p class="card-text">Fans are keen to know their favourite player ranking</p>
+                            <a href="#" class="btn btn-primary">Go to player stats</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    </center>
     <br>
-    <h1>hhhh</h1>,nsfjnnjnjwn
-    <h2>Hello </h2>
     
 
     
